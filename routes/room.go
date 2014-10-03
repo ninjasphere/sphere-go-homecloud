586b2f2743c07177e5ddd3d001a13317f132d11e
Mark Wolfe <mark@wolfe.id.au>
2014-10-03 10:01:54 +1000

    Fleshed out room api.

--- routes/room.go
+++ routes/room.go
@@ -21,6 +21,9 @@ func (lr *RoomRouter) Register(r martini.Router) {
 	r.Get("/", lr.GetAll)
 	r.Get("/:id", lr.GetRoom)
 	r.Delete("/:id", lr.DeleteRoom)
+	// r.Get("/:id/things", lr.GetThings) Not sure if this was used
+	r.Put("/:id/calibrate", lr.PutCalibrateRoom)
+	r.Put("/:id/apps/:appName", lr.PutAppRoomMessage)
 
 }
 
@@ -71,4 +74,48 @@ func (lr *RoomRouter) DeleteRoom(params martini.Params, w http.ResponseWriter, r
 	}
 
 	w.WriteHeader(http.StatusOK) // TODO: talk to theo about this response.
+}
+
+func (lr *RoomRouter) PutCalibrateRoom(params martini.Params, w http.ResponseWriter, roomModel *homecloud.RoomModel) {
+
+	room, err := roomModel.Fetch(params["id"])
+
+	log.Infof(spew.Sprintf("room: %v", room))
+
+	if err == homecloud.RecordNotFound {
+		WriteServerErrorResponse(fmt.Sprintf("Unknown room id: %s", params["id"]), http.StatusNotFound, w)
+		return
+	}
+
+	if err != nil {
+		WriteServerErrorResponse("Unable to retrieve room", http.StatusInternalServerError, w)
+		return
+	}
+
+	// TODO: Need to send message over an RPC thing
+	// LocationManager.calibrate(req.params.id, req.body.device, req.body.reset||false, function(err) {
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (lr *RoomRouter) PutAppRoomMessage(params martini.Params, w http.ResponseWriter, roomModel *homecloud.RoomModel) {
+
+	room, err := roomModel.Fetch(params["id"])
+
+	log.Infof(spew.Sprintf("room: %v", room))
+
+	if err == homecloud.RecordNotFound {
+		WriteServerErrorResponse(fmt.Sprintf("Unknown room id: %s", params["id"]), http.StatusNotFound, w)
+		return
+	}
+
+	if err != nil {
+		WriteServerErrorResponse("Unable to retrieve room", http.StatusInternalServerError, w)
+		return
+	}
+
+	// TODO: Need to send message over an RPC thing
+	//   req.bus.publish(topic,req.params.id,message,function(err,response){
+
+	w.WriteHeader(http.StatusOK)
 }
