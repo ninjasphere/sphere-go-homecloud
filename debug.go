7e0d2d69b51bf33c3dacd2783fda7e79b06d9d89
Mark Wolfe <mark@wolfe.id.au>
2014-10-10 14:58:19 +1100

    Added bugsnag.

---
+++ debug.go
@@ -0,0 +1,18 @@
+// +build !release
+
+package main
+
+import (
+	"github.com/bugsnag/bugsnag-go"
+	"github.com/juju/loggo"
+	"github.com/ninjasphere/go-ninja/logger"
+)
+
+func init() {
+	logger.GetLogger("").SetLogLevel(loggo.DEBUG)
+
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey:       "9d4e8ee05e1f9501a5b09ad982c5fc7f",
+		ReleaseStage: "development",
+	})
+}
