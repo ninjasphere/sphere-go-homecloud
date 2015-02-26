521f41725f3991d4610979b942242f7f06bd4caf
Mark Wolfe <mark@wolfe.id.au>
2015-02-27 10:34:40 +1100

    Remove unnecessary logger related calls.

--- debug.go
+++ debug.go
@@ -2,15 +2,9 @@
 
 package main
 
-import (
-	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
-)
+import "github.com/bugsnag/bugsnag-go"
 
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.DEBUG)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "9d4e8ee05e1f9501a5b09ad982c5fc7f",
 		ReleaseStage: "development",
